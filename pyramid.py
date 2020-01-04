0d8e6d57dcc76e524a0dac5dbb5e1d1c2d5ae037
martingoodall24 <13169366+martingoodall24@users.noreply.github.com>
2020-01-04 09:41:01 -0500

    removed rowz

--- pyramid.py
+++ pyramid.py
@@ -13,8 +13,7 @@ def print_pyramid(rows):
     """Print a pyramid of a given height
     :param int rows: total height
     """
-    # uncomment this!!  n = rows
-    n = rowz
+    n = rows
     width = n + (n-1)
     a = "="
     b = "-"
@@ -27,10 +26,6 @@ def print_pyramid(rows):
         q= z.center(width-i,"=")
         q2 = '{s:{c}^{n}}'.format(s=q,n=width,c=b)
         print(q2)
-        #output.append((q2 + '\n'))
-        #output = (q2 + '\n')
-    #return list(output)
-    
     #raise NotImplementedError("Called with rows={}".format(rows))
 
 if __name__ == "__main__":
