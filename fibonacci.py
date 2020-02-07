aa7ad7424de1977a43806e711f6ea56107a6ff72
martingoodall24 <13169366+martingoodall24@users.noreply.github.com>
2020-02-07 09:51:14 -0500

    testing

--- fibonacci.py
+++ fibonacci.py
@@ -3,49 +3,25 @@
 
 def last_8(some_int):
     """Return the last 8 digits of an int
+
     :param int some_int: the number
     :rtype: int
     """
-    last_8_digits = some_int % 100000000
-    return last_8_digits
-    #raise NotImplementedError()
-    
-def remove_me():
-    print("remove_me3")
+
+    raise NotImplementedError()
+
 
 def optimized_fibonacci(f):
-    out = []
-    out.append(0) 
-    out.append(1)
-    for i in range(2,f+1):
-        out.append(out[i-1] + out[i-2])
-    return out[f]
-    #raise NotImplementedError()
+    raise NotImplementedError()
 
-def optimized_fibonacci(f):
-    out = []
-    out.append(0) 
-    out.append(1)
-    for i in range(2,f+1):
-        out.append(out[i-1] + out[i-2])
-    return out[f]
-    #raise NotImplementedError()    
 
 class SummableSequence(object):
     def __init__(self, *initial):
-        self.initial = initial
-        #print(self.initial)
-        #raise NotImplementedError()
+        raise NotImplementedError()
 
     def __call__(self, i):
-        out = []
-        for each in self.initial:
-            #print(each)
-            out.append(each)
-        for j in range(2,i+1):
-            out.append(out[j-1] + out[j-2] + out[j-3])
-        return out[i]
-        #raise NotImplementedError()
+        raise NotImplementedError()
+
 
 if __name__ == "__main__":
 
@@ -53,3 +29,4 @@ if __name__ == "__main__":
 
     new_seq = SummableSequence(5, 7, 11)
     print("new_seq(100000)[-8:]:", last_8(new_seq(100000)))
+
